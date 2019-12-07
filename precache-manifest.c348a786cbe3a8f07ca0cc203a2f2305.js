0e96d84d6b671c2b3123f9f255e2206b68f4e8f9
NikitaKS <pancxxx2@gmail.com>
2019-12-07 14:26:00 +0100

    Updates

---
+++ precache-manifest.c348a786cbe3a8f07ca0cc203a2f2305.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4498b1e0a557071d2142c82a7c6b359",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "933aa4b172649137aff4",
+    "url": "/Counter/static/css/main.93e66efb.chunk.css"
+  },
+  {
+    "revision": "4b29dbb8ecc5b938acfe",
+    "url": "/Counter/static/js/2.7ad205b9.chunk.js"
+  },
+  {
+    "revision": "933aa4b172649137aff4",
+    "url": "/Counter/static/js/main.52e1e211.chunk.js"
+  },
+  {
+    "revision": "cd6e87301d7e441c1fae",
+    "url": "/Counter/static/js/runtime~main.130d950d.js"
+  }
+]);
